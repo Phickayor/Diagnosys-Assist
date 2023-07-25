76079f4b1480e167d101973e237a8e0fd8a19f99
Fikayo Jetawo <jetawof@gmail.com>
2023-07-25 22:29:39 +0100

    encryption and decryptio`n of password

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,5 +1,6 @@
 const connectToDatabase = require("../config/mongoconfig");
 const { generateToken } = require("../utilities/authorization.utilities");
+const {encryptPassword,deCryptPassword} = require("../utilities/password.utilities");
 const existingUser = async (req, res, next) => {
   var db = await connectToDatabase();
   var usersCollection = await db.collection("Users");
@@ -8,17 +9,19 @@ const existingUser = async (req, res, next) => {
   });
   if (result != undefined) {
     console.log("Existing User");
-    res.status(409).json({ message: "Existing User" });
+    res.status(409).json({ message: "Existing User"});
   } else {
     next();
   }
 };
 
 const register = async (req, res, next) => {
+  var mailValue = req.body.email.toLowerCase()
+ var hashed = await encryptPassword(req.body.pswd);
   var info = {
     name: req.body.name,
-    email: req.body.email,
-    pswd: req.body.pswd,
+    email: mailValue,
+    pswd: hashed,
     category: req.body.category,
     phoneNumber: req.body.phoneNumber,
     address: req.body.address,
@@ -34,8 +37,9 @@ const login = async (req, res, next) => {
   var { email, pswd } = req.body;
   var db = await connectToDatabase();
   var usersCollection = await db.collection("Users");
-  const result = await usersCollection.findOne({ email: email });
-  if (result.email === email && result.pswd === pswd) {
+  const result = await usersCollection.findOne({ email: email.toLowerCase() });
+  const decrypted = await deCryptPassword(pswd, result.pswd); 
+  if (result.email === email && decrypted.success ===true) {
     const { token, message, success } = await generateToken(
       result._id,
       result.name,
@@ -46,7 +50,7 @@ const login = async (req, res, next) => {
       ? res.status(200).json({ token, message })
       : res.status(503).json({ message, error: true });
   } else {
-    res.status(404).json({ message: "non-existing account" });
+    res.status(404).json({ message: "non-existing account"});
   }
 };
 
---
+++ server/utilities/password.utilities.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const encryptPassword = async (pswd) => {
+  try {
+    let salt = await bcrypt.genSalt(saltRounds);
+    let hashedPassword = await bcrypt.hash(pswd, salt);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deCryptPassword = async (pswd, hash) => {
+  try {
+    const match = await bcrypt.compare(pswd, hash);
+    if (match) {
+      return {
+        match,
+        success: true
+      };
+    } else {
+      return {
+        message: "Password doesn't match",
+        success: false
+      };
+    }
+  } catch (error) {
+    return {
+      message: error.message,
+      success: false
+    };
+  }
+};
+module.exports = { encryptPassword, deCryptPassword };
