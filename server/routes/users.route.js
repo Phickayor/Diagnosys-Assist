f2e13017a922e4b71b0bddd2f9d1285d260c57d9
Fikayo Jetawo <jetawof@gmail.com>
2023-07-28 00:46:25 +0100

    otp generator

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { register, existingUser, login } = require("../controllers/users.controller");
+const {
+  register,
+  existingUser,
+  login
+} = require("../controllers/users.controller");
 const { checkToken } = require("../utilities/authorization.utilities");
 const validator = require("../utilities/emailvalidator.utilities");
 const verificationmail = require("../utilities/emailverification.utilities");
@@ -9,5 +13,5 @@ router.post("/register", existingUser, register);
 router.post("/login", login);
 router.post("/verifytoken", checkToken);
 router.post("/verifytoken", checkToken);
-router.post("/verifyemail", validator, verificationmail);
+router.post("/verifyemail", verificationmail);
 module.exports = router;
--- server/utilities/emailverification.utilities.js
+++ server/utilities/emailverification.utilities.js
@@ -1,19 +1,21 @@
 const transporter = require("../config/transporterconfig");
+const generateOtp = require("./otpgenerator");
 
 const verificationmail = async (req, res) => {
   var email = req.body.email;
   email.toLowerCase();
+  var otp = generateOtp();
   var mailOptions = {
     from: `"Diagnosyst Assist" <${process.env.MAIL_USERNAME}>`,
     to: email,
     subject: "Verify your email on Diagnosys Assist",
-    text: "Got Here"
+    text: `Your OTP is : ${otp}`
   };
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
       console.log(err);
     } else {
-      console.log("Email sent: " + info.response);
+      console.log("Email sent: " + info.response + otp);
       res.json({ info: "success" });
     }
   });
