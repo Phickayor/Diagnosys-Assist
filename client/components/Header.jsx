37131694b8e57a2ff11a9d9e7cac92c003fce37b
Fikayo Jetawo <jetawof@gmail.com>
2023-09-26 20:00:15 +0100

    Header component

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,33 +1,46 @@
-import {faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
 function Header() {
-  const menu = <FontAwesomeIcon icon={faBars} className="cursor-pointer text-xl"/>
+  const bars = (
+    <FontAwesomeIcon icon={faBars} className="cursor-pointer self-center" />
+  );
+  const profile = (
+    <FontAwesomeIcon
+      icon={faUser}
+      className="cursor-pointer self-center px-3"
+    />
+  );
+
   return (
-    <div className="sticky top-0 backdrop-blur-md flex py-6 lg:px-10 px-5 text-black justify-between">
-      <h1 className="text-xl lg:text-2xl font-semibold self-center">Diagnosys Assist</h1>
-      <ul className="hidden lg:flex gap-x-4 self-center font-medium text-lg">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+    <header className="fixed top-0 z-20 bg-deepGreen text-white  py-4 border-b-2 w-full flex justify-between lg:px-20 px-5">
+      {/* Logo */}
+      <h1 className="self-center lg:text-lg font-medium text-lightGray">
+        Diagnosys
+      </h1>
+      <ul className="hidden lg:flex space-x-8 text-sm self-center">
+        <li className="hover:border-b-4 border-green-600 duration-100">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:border-b-4 border-green-600 duration-100">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="hover:border-b-4 border-green-600 duration-100">
+          <Link href="/services">Services</Link>
+        </li>
+        <li className="hover:border-b-4 border-green-600 duration-100">
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li className="hover:border-b-4 border-green-600 duration-100">
+          <Link href="/blogs">Blogs</Link>
+        </li>
       </ul>
-      <div className="hidden lg:flex gap-x-4">
-        <Link
-          href="/auth/login"
-          className="text-lg border-2 border-slate-500 px-8 py-2 text-black font-semibold rounded-lg"
-        >
-          Login
-        </Link>
-        <Link
-          href="/auth/register"
-          className="text-lg bg-blue-500 px-8 py-2 font-semibold rounded-lg text-white"
-        >
-          Sign Up
-        </Link>
-      </div>
-      <div className="lg:hidden self-center">{menu}</div>
-    </div>
+      <button className="hidden lg:block hover:scale-105 text-sm duration-300 cursor-pointer rounded-md bg-green-600 text-white px-3 py-2.5 font-medium">
+        <Link href="/auth/">Account{profile}</Link>
+      </button>
+      <div className="lg:hidden">{bars}</div>
+    </header>
   );
 }
 
